947d7c133459629c9851f248c427fd9a43003e2a
qizongliang <QIZONG.LIANG59@myhunter.cuny.edu>
2021-04-18 00:51:29 -0400

    First Login Stuff

---
+++ app/src/main/java/com/example/rentahome/ParseApplication.java
@@ -0,0 +1,11 @@
+package com.example.rentahome;
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+
+public class ParseApplication {
+}
