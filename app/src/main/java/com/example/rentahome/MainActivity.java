cc5ba3ab6fbd499773957d5da9c237dabe02aa96
qizongliang <QIZONG.LIANG59@myhunter.cuny.edu>
2021-04-24 23:22:58 -0400

    fragment

--- app/src/main/java/com/example/rentahome/MainActivity.java
+++ app/src/main/java/com/example/rentahome/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.rentahome;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuitem) {
+                Fragment fragment;
+                switch (menuitem.getItemId()){
+                    case R.id.nav_home:break;
+                    case R.id.profile: break;
+                    case R.id.options: break;
+                }
+
+                return true;
+            }
+        });
     }
 }
