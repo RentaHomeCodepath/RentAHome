7c50511e1282d00d2485f418ecb7ed7289e8cf9c
fahadtahir02 <fahad.tahir79@myhunter.cuny.edu>
2021-05-06 03:00:54 -0400

    Updated UI/UX

--- app/src/main/java/com/example/rentahome/StartingActivity.java
+++ app/src/main/java/com/example/rentahome/StartingActivity.java
@@ -5,10 +5,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import com.google.android.material.tabs.TabLayout;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
@@ -20,9 +23,23 @@ public class StartingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.starting_activity);
 
-        Button btnlogin = findViewById(R.id.LoginBtn);
+        TabLayout tabLayout;
+
+
+
+        EditText et_Username = findViewById(R.id.etUsername_login);
+        EditText et_password = findViewById(R.id.etPassword_login);
+        Button btn_login = findViewById(R.id.btnLogin_login);
+
         Button btnsignup = findViewById(R.id.SignupBtn);
         Button btnGuest = findViewById(R.id.GuestBtn);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login(et_Username.getText().toString(), et_password.getText().toString());
+            }
+        });
 
         btnGuest.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,21 +51,15 @@ public class StartingActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {goSignupActivity();}
         });
-        btnlogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {gologinActivity();}
-        });
+
     }
     private void goSignupActivity(){
         Intent intent = new Intent(this, signupActivity.class);
         startActivity(intent);
         finish();
     }
-    private void gologinActivity(){
-        Intent intent = new Intent(this, loginActivity.class);
-        startActivity(intent);
-        finish();
-    }
+
+
     private void goMainActivity(){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
@@ -69,5 +80,19 @@ public class StartingActivity extends AppCompatActivity {
             }
         });
     }
+    private void login(String username, String password) {
 
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null) {
+                    Toast.makeText(StartingActivity.this, "Login Failed", Toast.LENGTH_SHORT);
+                    return;
+                }
+                goMainActivity();
+                Toast.makeText(StartingActivity.this, "Success!", Toast.LENGTH_SHORT);
+            }
+        });
+
+    }
 }
